16c0daa9a9a9a697625ccba5c8d99adfd3c77bec
dhearr <89125079+dhearr@users.noreply.github.com>
2025-08-05 12:09:57 +0700

    Update Tiktok.jsx

--- src/components/Tiktok.jsx
+++ src/components/Tiktok.jsx
@@ -3,7 +3,7 @@ import { tiktok } from "../../public/static/images";
 const Tiktok = () => {
   return (
     <a
-      href="https://www.tiktok.com/@codewithstyle?_t=8hljWUMFoly&_r=1"
+      href="https://www.tiktok.com/@tukangketikk"
       target="blank"
     >
       <img src={tiktok} alt="" className="w-[50px] h-[50px]" />
